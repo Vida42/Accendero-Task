a0a6521d6f5ba5bde11c6a9014a3306bf7a1adf2
Mugen <leo.t5@outlook.com>
2021-08-12 22:57:00 -0400

    new models and home page

--- myblog/models.py
+++ myblog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    summary = models.CharField(max_length=200, blank=True)
+    body = models.TextField()
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.title
--- myblog/views.py
+++ myblog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post, Tag
 
 # Create your views here.
+def index(request):
+    context = {
+        'title': 'MyBlog',
+        'word': "Welcome To My Blog!"
+    }
+    return render(request, 'home.html', context)
